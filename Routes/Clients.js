3dbd11e1529214995c9bcce04f792f1be7853744
Godwin Burume <godwinnyembo@gmail.com>
2024-01-04 14:54:52 +0100

    init

--- Routes/Clients.js
+++ Routes/Clients.js
@@ -15,7 +15,7 @@ router.post('/', limits(8000, 15), authUser, validatorClient, clientsCtrl.addOne
 router.put('/:idClient', limits(8000, 15), existClient, authUser, validatorClient, clientsCtrl.updateOneClient); // Modifie un client, uniquement par l'administrateur
 
 router.get('/:idClient', limits(8000, 15), existClient, authUser, clientsCtrl.getOneClient); // Récupère un client
-router.get('/', limits(8000, 15), authUser, clientsCtrl.getAllClient); // Récupère tout les clients, par tout le monde
+router.get('/', limits(8000, 15), clientsCtrl.getAllClient); // Récupère tout les clients, par tout le monde
 
 router.delete('/:idClient', limits(8000, 15), existClient, authUser, clientsCtrl.deleteOneClient); // Supprime un client, uniquement par l'administrateur
 
